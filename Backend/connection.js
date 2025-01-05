ee42a37516b281a063b702d2566f51b754bfdd2f
Lacho18 <121165891+Lacho18@users.noreply.github.com>
2025-01-05 17:55:26 +0200

    Update connection.js

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,20 +1,3 @@
-/*const { Client } = require('pg');
-require('dotenv').config();
-
-const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: process.env.DATABASE_PASSWORD,
-    database: "school_system"
-});
-
-client.connect()
-    .then(() => console.log("Connection complete"))
-    .catch(err => console.log("Connection error: " + err.stack));
-
-module.exports = client;*/
-
 const { Client } = require('pg');
 require('dotenv').config();
 
